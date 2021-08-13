4c4c68c3edcf6a11bdc4b32078178216e04e3a78
Vlad Kryshchuk <vladkryshchuk20@gmail.com>
2021-08-13 17:50:26 +0300

    first commit

---
+++ src/component/Snake/SnakeGame.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import "./style.css";
+import Snake from "./Snake";
+import Food from "./Food";
+
+const getRandomCoordinates = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  let y = Math.floor((Math.random() * (max - min + 1) + min) / 2) * 2;
+  return [x, y];
+};
+
+class SnakeGame extends Component {
+  state = {
+    food: getRandomCoordinates(),
+    speed: 200,
+    direction: "RIGHT",
+    SnakeDots: [
+      [0, 0],
+      [2, 0],
+    ],
+  };
+
+  componentDidMount() {
+    setInterval(this.moveSnake, this.state.speed);
+    document.onkeydown = this.onKeyDown;
+  }
+
+  componentDidUpdate() {
+    this.checkIfOutOfBorders();
+  }
+
+  onKeyDown = (e) => {
+    e = e || window.event;
+    switch (e.keyCode) {
+      case 38:
+        this.setState({ direction: "UP" });
+        break;
+      case 40:
+        this.setState({ direction: "DOWN" });
+        break;
+      case 37:
+        this.setState({ direction: "LEFT" });
+        break;
+      case 39:
+        this.setState({ direction: "RIGHT" });
+        break;
+    }
+  };
+
+  moveSnake = () => {
+    let dots = [...this.state.SnakeDots];
+    let head = dots[dots.length - 1];
+
+    switch (this.state.direction) {
+      case "RIGHT":
+        head = [head[0] + 2, head[1]];
+        break;
+      case "LEFT":
+        head = [head[0] - 2, head[1]];
+        break;
+      case "DOWN":
+        head = [head[0], head[1] + 2];
+        break;
+      case "UP":
+        head = [head[0], head[1] - 2];
+        break;
+    }
+    dots.push(head);
+    dots.shift();
+    this.setState({
+      SnakeDots: dots,
+    });
+  };
+
+  checkIfOutOfBorders() {
+    let head = this.state.snakeDots[this.state.SnakeDots.length - 1];
+    if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0) {
+      this.onGameOver();
+    }
+  }
+
+  onGameOver() {
+    alert(`Game Over. Snake length is ${this.state.SnakeDots.length}`);
+  }
+
+  render() {
+    return (
+      <div className="game_area">
+        <Snake snakeDots={this.state.SnakeDots} />
+        <Food dot={this.state.food} />
+      </div>
+    );
+  }
+}
+
+export default SnakeGame;
